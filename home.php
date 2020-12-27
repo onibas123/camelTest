7cdc5a1cb4041191d0e8312c34fbfed2fcaf1d47
Matias <mquezadadev@gmail.com>
2020-12-27 06:51:59 -0300

    fixed validate !empty access_way auth

--- home.php
+++ home.php
@@ -26,7 +26,13 @@
         <div class="collapse navbar-collapse navbar-ex1-collapse">
             <ul class="nav navbar-nav navbar-right">
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Último acceso <?php echo $_SESSION['user_data']['last_access'].' | '.$_SESSION['user_data']['access_way'];?><b class="caret"></b>
+                    <?php
+                        $access_way = 'WEB';
+                        if(!empty($_SESSION['user_data']['access_way']))
+                            $access_way = $_SESSION['user_data']['access_way'];
+                        
+                    ?>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Último acceso <?php echo $_SESSION['user_data']['last_access'].' | '.$access_way;?><b class="caret"></b>
                     </a>
                     <ul class="dropdown-menu">
                     <li><a><?php echo $_SESSION['user_data']['rol'];?></a></li>
